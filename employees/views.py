4a4c4921a6f7998800f058bc84aae8511e798afd
Dorota Zelazkiewicz <dorota.elen@gmail.com>
2019-09-05 13:07:45 +0200

    views init

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Employee, Position
+from rest_framewrk import viewsets
+from .serializers import EmployeeSerializer, PositionSerializer
 # Create your views here.
+
+
+class PositionViewSet(viewsets.ModelViewSet):
+    queryset = Position.objects.all()
+    serializer_class = PositionSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all().order_by('surname')
+    serializer_class = EmployeeSerializer
+    
