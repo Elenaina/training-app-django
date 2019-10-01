be204b7431372df601954e3d63ef13a18336be10
Michał Żelazkiewicz <michal.zelazkiewicz@gmail.com>
2019-10-01 11:36:44 +0200

    Fix CORS issue

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = [
     'localhost',
 ]
 
+CORS_ORIGIN_WHITELIST = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+]
+
 
 # Application definition
 
@@ -41,12 +46,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'employees',
     'polls.apps.PollsConfig',
-    'rest_framework'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
